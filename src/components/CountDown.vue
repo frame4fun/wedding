db187006bdcb7d14b28bec7d75188620b9dea769
Guillaume Rahbari <guillaume.rahbari@protonmail.com>
2022-05-01 14:59:12 +0200

    feat: shape heart

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -19,6 +19,12 @@ export default {
   <div class="countdown">
     <vue-countdown :time="time" v-slot="{ days }">
       <div class="column">
+        <svg class="heart" viewBox="0 0 32 29.6">
+          <path
+            d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
+          />
+        </svg>
         <span class="daysLeft">{{ days }}</span>
         <h3 class="text">jours</h3>
       </div>
@@ -41,9 +47,6 @@ export default {
   align-items: center;
   height: 150px;
   width: 150px;
-  border-radius: 100%;
-  border: 3px dotted hsla(var(--pink), 1);
-  background-color: hsla(var(--pink), 0.8);
 }
 
 .daysLeft {
@@ -53,5 +56,26 @@ export default {
 
 .text {
   font-size: 1.7rem;
+}
+
+.heart {
+  fill: hsla(var(--pink), 0.8);
+  position: relative;
+  top: -7px;
+  width: 200px;
+  animation: pulse 1s ease infinite;
+  position: absolute;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
