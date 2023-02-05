193e90028445108204fe151195b3f73e3f116580
GuiGui <guillaume.rahbari@protonmail.com>
2023-02-05 22:13:15 +0100

    fix: update available rooms

--- src/components/castleRooms.ts
+++ src/components/castleRooms.ts
@@ -1,6 +1,3 @@
-import Chambertin from '../assets/castleRooms/chambertin.webp';
-import Richebourg from '../assets/castleRooms/richebourg.webp';
-import LesClos from '../assets/castleRooms/lesclos.webp';
 import Montrachet from '../assets/castleRooms/montrachet.webp';
 import Presidentielle from '../assets/castleRooms/presidentielle.webp';
 import Double from '../assets/castleRooms/double.webp';
@@ -24,26 +21,8 @@ export interface CastleCategory {
 export const castleCategories: CastleCategory[] = [
   {
     title: 'Les chambres du château',
-    description: '6 chambres très spacieuses au 1er étage du Château',
+    description: '2 chambres très spacieuses au 1er étage du Château',
     rooms: [
-      {
-        Nom: 'Chambre Chambertin',
-        Info: 'Chambre spacieuse avec vue sur le parc et la forêt.',
-        Prix: 245,
-        Image: Chambertin,
-      },
-      {
-        Nom: 'Chambre Richebourg',
-        Info: 'Chambre double avec vue sur le Jardin à la française',
-        Prix: 245,
-        Image: Richebourg,
-      },
-      {
-        Nom: 'Chambre des Clos',
-        Info: 'Chambre qui peut également convenir pour les personnes à mobilité réduite.',
-        Prix: 200,
-        Image: LesClos,
-      },
       {
         Nom: 'Chambre Montrachet',
         Info: 'Chambre double avec vue sur le jardin à la française.',
@@ -60,7 +39,7 @@ export const castleCategories: CastleCategory[] = [
   },
   {
     title: 'Les chambres de la dépendance',
-    description: '16 chambres doubles de 14m2 et 1 chambre quadruple de 20m2',
+    description: '6 chambres doubles de 14m2 et 1 chambre quadruple de 20m2',
     rooms: [
       {
         Nom: 'Chambre double',
